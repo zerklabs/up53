520b39b6659f53c266862c73ed3c120ac85416cf
Robin cabrel Harper <cabrel@zerklabs.com>
2015-01-03 12:12:22 -0500

    added docker steps to build script

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 
 printf "** Building linux/amd64\n"
 go build -a -o bin/linux-amd64/up53 github.com/zerklabs/up53
+
+printf "** Building docker container\n"
+docker build --no-cache -t cabrel/up53 .
+docker push cabrel/up53 .
